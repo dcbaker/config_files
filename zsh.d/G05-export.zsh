f44050468e5afc8de7c21556760538ad87c4138a
Dylan Baker <baker.dylan.c@gmail.com>
2014-07-25 16:16:30 -0700

    zsh: change exports to work for gentoo
    Really I need a solution to do stuff based on the distro that I'm
    running on, but that's hard

--- zsh.d/G05-export.zsh
+++ zsh.d/G05-export.zsh
@@ -2,11 +2,12 @@
 
 export EDITOR="/usr/bin/vim"
 export TERM="screen-256color"
-export PATH="/usr/lib/ccache:/sbin:/usr/sbin:$PATH:$HOME/.local/bin:$HOME/.gem/ruby/2.0.0/bin"
+export PATH="$(/usr/bin/ruby -rubygems -e 'puts Gem.user_dir')/bin:/usr/lib/ccache/bin:/sbin:/usr/sbin:$PATH:$HOME/.local/bin"
 export PKG_CONFIG_PATH="/usr/local/lib/x86_64-linux-gnu/pkgconfig/:$PKG_CONFIG_PATH"
 export SSH_ASKPASS="/usr/bin/ksshaskpass"
-export LIBGL_DRIVERS_PATH="/usr/local/lib/x86_64-linux-gnu/dri:/usr/local/lib/i386-linux-gnu/dri:$LIBGL_DRIVERS_PATH"
-export LD_LIBRARY_PATH="/usr/local/lib/x86_64-linux-gnu:/usr/local/lib/i386-linux-gnu:$LD_LIBRARY_PATH"
+export LIBGL_DRIVERS_PATH="/usr/local/lib64/dri:/usr/local/lib32/dri:$LIBGL_DRIVERS_PATH"
+export GBM_DRIVERS_PATH=$LIBGL_DRIVERS_PATH
+export LD_LIBRARY_PATH="/usr/local/lib32:/usr/local/lib64:$LD_LIBRARY_PATH"
 export GOPATH="$HOME/golang"
 
 # vim: ft=sh
