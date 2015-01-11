ff26f90cc12deb28831d446459b5e6108e0dbe72
Dylan Baker <baker.dylan.c@gmail.com>
2015-01-10 22:43:40 -0800

    zsh: use zkbd for keybindings.
    Instead of trying to set all of the keys manually which didn't work most
    of the time, use zkbd.
    
    To generate a new mapping:
    autoload zkbd && zkbd
    
    For any keys that cannot be pressed (like f11 or menu) use space to
    skip.
    
    The file will be stored in ~/.zkbd/${TERM}-${DISPLAY}, remove the
    -${DISPLAY}

--- zsh.d/G03-keybindings.zsh
+++ zsh.d/G03-keybindings.zsh
@@ -1,43 +1,14 @@
-# create a zkbd compatible hash;
-# to add other keys to this hash, see: man 5 terminfo
-typeset -A key
+autoload zkbd
+source ~/.zkbd/$TERM
 
-key[Home]=${terminfo[khome]}
-
-key[End]=${terminfo[kend]}
-key[Insert]=${terminfo[kich1]}
-key[Delete]=${terminfo[kdch1]}
-key[Up]=${terminfo[kcuu1]}
-key[Down]=${terminfo[kcud1]}
-key[Left]=${terminfo[kcub1]}
-key[Right]=${terminfo[kcuf1]}
-key[PageUp]=${terminfo[kpp]}
-key[PageDown]=${terminfo[knp]}
-
-# setup key accordingly
-[[ -n "$key[Home]"     ]]  && bindkey  "$key[Home]"      beginning-of-line
-[[ -n "$key[End]"      ]]  && bindkey  "$key[End]"       end-of-line
-[[ -n "$key[Insert]"   ]]  && bindkey  "$key[Insert]"    overwrite-mode
-[[ -n "$key[Delete]"   ]]  && bindkey  "$key[Delete]"    delete-char
-[[ -n "$key[Up]"       ]]  && bindkey  "$key[Up]"        up-line-or-history
-[[ -n "$key[Down]"     ]]  && bindkey  "$key[Down]"      down-line-or-history
-[[ -n "$key[Left]"     ]]  && bindkey  "$key[Left]"      backward-char
-[[ -n "$key[Right]"    ]]  && bindkey  "$key[Right]"     forward-char
-[[ -n "$key[PageUp]"   ]]  && bindkey  "$key[PageUp]"    history-beginning-search-backward
-[[ -n "$key[PageDown]" ]]  && bindkey  "$key[PageDown]"  history-beginning-search-forward
-
-# Finally, make sure the terminal is in application mode, when zle is
-# active. Only then are the values from $terminfo valid.
-
-function zle-line-init () {
-    echoti smkx 2&>/dev/null
-}
-
-function zle-line-finish () {
-    echoti rmkxx 2&>/dev/null
-}
-
-zle -N zle-line-init
-zle -N zle-line-finish
-
-# vim: ft=zsh
+[[ -n ${key[Backspace]} ]]  && bindkey "${key[Backspace]}" backward-delete-char
+[[ -n ${key[Insert]} ]]     && bindkey "${key[Insert]}" overwrite-mode
+[[ -n ${key[Home]} ]]       && bindkey "${key[Home]}" beginning-of-line
+[[ -n ${key[PageUp]} ]]     && bindkey "${key[PageUp]}" up-line-or-history
+[[ -n ${key[Delete]} ]]     && bindkey "${key[Delete]}" delete-char
+[[ -n ${key[End]} ]]        && bindkey "${key[End]}" end-of-line
+[[ -n ${key[PageDown]} ]]   && bindkey "${key[PageDown]}" down-line-or-history
+[[ -n ${key[Up]} ]]         && bindkey "${key[Up]}" up-line-or-search
+[[ -n ${key[Left]} ]]       && bindkey "${key[Left]}" backward-char
+[[ -n ${key[Down]} ]]       && bindkey "${key[Down]}" down-line-or-search
+[[ -n ${key[Right]} ]]      && bindkey "${key[Right]}" forward-char
