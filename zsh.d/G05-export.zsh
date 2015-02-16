0aecdc416d33414ea609672d4cb3126b4a6007d4
Dylan Baker <baker.dylan.c@gmail.com>
2015-02-15 16:37:52 -0800

    zsh/G05-export: don't set mes overrides

--- zsh.d/G05-export.zsh
+++ zsh.d/G05-export.zsh
@@ -5,9 +5,6 @@ export TERM="screen-256color"
 export PATH="$(/usr/bin/ruby -rubygems -e 'puts Gem.user_dir')/bin:/usr/lib/ccache/bin:/sbin:/usr/sbin:$PATH:$HOME/.local/bin"
 export PKG_CONFIG_PATH="/usr/local/lib/x86_64-linux-gnu/pkgconfig/:$PKG_CONFIG_PATH"
 export SSH_ASKPASS="/usr/bin/ksshaskpass"
-export LIBGL_DRIVERS_PATH="/usr/local/lib64/dri:/usr/local/lib32/dri:$LIBGL_DRIVERS_PATH"
-export GBM_DRIVERS_PATH=$LIBGL_DRIVERS_PATH
-export LD_LIBRARY_PATH="/usr/local/lib32:/usr/local/lib64:$LD_LIBRARY_PATH"
 export GOPATH="$HOME/golang"
 
 # vim: ft=sh
