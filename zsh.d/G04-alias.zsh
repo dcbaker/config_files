4af03dd507e4968c827977d6819731d31d8df2df
Dylan Baker <baker.dylan.c@gmail.com>
2015-10-10 13:07:37 -0700

    zsh.d/G04-alias.zsh: Add alias for systemctl --user

--- zsh.d/G04-alias.zsh
+++ zsh.d/G04-alias.zsh
@@ -11,6 +11,8 @@ alias suls="sudo ls"
 alias suless="sudo less -S"
 alias sucat="sudo cat"
 
+alias usystemctl="systemctl --user"
+
 # Force ssh-add to use a gui agent
 alias ssh-add="ssh-add </dev/null"
 
